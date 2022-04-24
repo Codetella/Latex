6e1bc047ca5a639d00a7ff06024f48a8f67cd4fc
Codetella <www.twn.96@hotmail.com>
2022-04-24 13:28:03 +0200

    first comm

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//clear
+
+document.querySelector("#psi").addEventListener("click", ()=>{
+
+    document.querySelector("#display").value = " \\psi";
+})
+
+
+
+document.querySelector("#ohm").addEventListener("click", ()=>{
+
+    document.querySelector("#display").value = "\\Omega";
+})
